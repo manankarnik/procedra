5b0b70f943a051a727e3b15d77d90343fd7112ed
Manan Karnik <manankarnik03@gmail.com>
2024-01-01 19:58:02 +0530

    feat: add dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import "../app.pcss";
+  import { ModeWatcher } from "mode-watcher";
+  import DarkModeToggleButton from "$lib/components/dark-mode-toggle-button.svelte";
 </script>
 
+<ModeWatcher />
+<DarkModeToggleButton />
 <slot />
