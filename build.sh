67ffe5d651e9f373823541b6ebe7dab9ff06a197
Manan Karnik <manankarnik03@gmail.com>
2024-01-10 23:31:11 +0530

    build(deploy): update script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 cd utils/
+rustup default stable
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
 cargo build --release --bin map --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ../src/utils/map --out-name index --target web ./target/wasm32-unknown-unknown/release/map.wasm
 cargo build --release --bin terrain --target wasm32-unknown-unknown
