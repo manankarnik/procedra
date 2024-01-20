52c43b508297862b2c8079c781634b6bd422ac9a
Manan Karnik <manankarnik03@gmail.com>
2024-01-21 00:09:28 +0530

    feat: add page loading progress bar

---
+++ src/lib/components/loading-bar.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { beforeNavigate, afterNavigate } from "$app/navigation";
+  let progress = "w-0";
+  let display = "hidden";
+  beforeNavigate(() => {
+    display = "visible";
+    setTimeout(() => {
+      progress = "w-[40%]";
+    }, 0);
+  });
+  afterNavigate(() => {
+    progress = "w-full";
+    setTimeout(() => {
+      display = "hidden";
+      progress = "w-0";
+    }, 200);
+  });
+</script>
+<div
+  class={`animate-gradient absolute top-0 z-20 h-1 w-0 bg-gradient-to-r transition-[width] duration-200 ${progress} ${display}`}
+/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
   import "../app.pcss";
   import { ModeWatcher } from "mode-watcher";
   import Header from "$lib/components/header.svelte";
+  import LoadingBar from "$lib/components/loading-bar.svelte";
 </script>
 
+<LoadingBar />
 <ModeWatcher />
 <Header />
 <slot />
